ee7010d418a6d0d9135d830acefc1af0fc21ec98
Mirailisc <mirailisclm@gmail.com>
2022-01-30 14:49:12 +0700

    fully finished

--- Arduino-composer.ino
+++ Arduino-composer.ino
@@ -1,10 +1,10 @@
-#include <FirebaseArduino.h>
-#include <ESP8266WiFi.h>
-#include <AccelStepper.h>
+#include <IOXhop_FirebaseESP32.h>
+#include <WiFi.h>
+#include <Stepper.h>
 
-#define LEDD0 D0 // LED 1
-#define LEDD1 D1 // LED 2
-#define LEDD2 D2 // LED 3
+#define LED1 23
+#define LED2 22
+#define LED3 21
 
 // WIFI
 #define WIFI_SSID "Poolnoi Family_2.4G"
@@ -16,22 +16,22 @@
 
 
 //Stepper Motor
-#define IN1 D5 // IN1 is connected to NodeMCU pin D1 (GPIO5)
-#define IN2 D6 // IN2 is connected to NodeMCU pin D2 (GPIO4)
-#define IN3 D7 // IN3 is connected to NodeMCU pin D3 (GPIO0)
-#define IN4 D8  // IN4 is connected to NodeMCU pin D4 (GPIO2)
+#define IN1 19
+#define IN2 18
+#define IN3 5
+#define IN4 17
 
 const int stepsPerRevolution = 2048;
-AccelStepper stepper(AccelStepper::HALF4WIRE, IN1, IN3, IN2, IN4);
+Stepper myStepper(stepsPerRevolution, IN1, IN3, IN2, IN4);
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
   
   //LED
-  pinMode(LEDD0, OUTPUT);
-  pinMode(LEDD1, OUTPUT);
-  pinMode(LEDD2, OUTPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
 
   //Wifi
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -52,37 +52,43 @@ void setup() {
   Serial.println("Connected to Firebase");
   Firebase.setString("status", "Ready"); // Set status to Stand by
   Firebase.setBool("isSkip", false); // Set isSkip to false
+
+  //Stepper
+  myStepper.setSpeed(15 );
 }
 
 void loop() {
   String status = Firebase.getString("status"); // Get status data from Firebase
   bool isSkip = Firebase.getBool("isSkip"); // Get skip data from Firebase
-  // int Times = 0;
 
   if (status == "Ready") {
     Serial.println("Machine is ready!");
-    digitalWrite(LEDD0, HIGH);
-    digitalWrite(LEDD1, LOW);
-    digitalWrite(LEDD2, LOW);
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
   } else if (status == "Working") {
     if (isSkip == false) {
       Serial.println("Machine is working!");
-      digitalWrite(LEDD0, LOW);
-      digitalWrite(LEDD1, HIGH);
-      digitalWrite(LEDD2, LOW);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, LOW);
       // check current stepper motor position to invert direction
-//      while(Times < 10) {
-//        
-//        if (stepper.distanceToGo() == 0){
-//          stepper.moveTo(-stepper.currentPosition());
-//          Serial.println("Changing direction");
-//        }
-//        // move the stepper motor (one step at a time)
-//        stepper.run();
-//        Times += 1;
-//        Serial.println(Times);
-//      }
-      Firebase.setString("status", "Need Water"); // Set status to Waiting after finish Motor
+
+      for (int i = 0; i <= 10; i++) {
+        Serial.println("clockwise");
+        myStepper.step(stepsPerRevolution);
+        for (int i = 0; i <= 1000; i++) {
+          delay(10);
+        }
+      
+        // step one revolution in the other direction:
+        Serial.println("counterclockwise");
+        myStepper.step(-stepsPerRevolution);
+        for (int i = 0; i <= 1000; i++) {
+          delay(10);
+        }
+      }
+      Firebase.setString("status", "Need Water");
     } else if (isSkip == true) {
       Serial.println("Skipped Motor");
       delay(2000);
@@ -91,9 +97,9 @@ void loop() {
 
   } else if (status == "Need Water") {
     Serial.println("Machine is waiting for water!");
-    digitalWrite(LEDD0, LOW);
-    digitalWrite(LEDD1, LOW);
-    digitalWrite(LEDD2, HIGH);
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, HIGH);
   } else {
     Firebase.setString("status", "Ready"); // Set status back to Ready when status is not in condition
   }
